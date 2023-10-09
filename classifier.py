190cd2a3de26d15869d6c9ca854354d5f24eb623
sergei.obj <s-obj@yandex.ru>
2023-10-09 13:26:14 +0200

    allow for distance <= 2 between lemmas' stems when classifying as morph

--- classifier.py
+++ classifier.py
@@ -423,7 +423,7 @@ def related_stems(first, second):
     min_length = min(len(first_stem), len(second_stem))
     return (distance(first_stem[:min_length], second_stem[:min_length]) < 2 or
             distance(first_stem[-min_length:], second_stem[-min_length:]) < 2 or
-            distance(first_stem, second_stem) == 2)
+            distance(first_stem, second_stem) <= 2)
 
 
 def morph(o_toks, c_toks):
